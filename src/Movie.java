3fd43d9e5a0f405de0e61127bd11869900e312ec
Mushrum-mmb <nambnp30@gmail.com>
2026-06-16 14:32:28 +0700

    dfssdf

--- src/Movie.java
+++ src/Movie.java
@@ -10,18 +10,6 @@ public class Movie {
 	private String link;
 	private boolean isVip;
 //	constructor movie
-	public Movie(int id, String nameMovie, String director, String actor, Category category, String country,
-			String link, boolean isVip) {
-		super();
-		this.id = id;
-		this.nameMovie = nameMovie;
-		this.director = director;
-		this.actor = actor;
-		this.category = category;
-		this.country = country;
-		this.link = link;
-		this.isVip = isVip;
-		
-	}
+	
 	
 }
