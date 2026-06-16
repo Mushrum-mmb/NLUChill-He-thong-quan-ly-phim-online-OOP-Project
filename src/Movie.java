a74c87a8a08b53a5d203c08529fffe33d56b2047
Mushrum-mmb <nambnp30@gmail.com>
2026-06-16 14:24:54 +0700

    Add movie

---
+++ src/Movie.java
@@ -0,0 +1,26 @@
+
+public class Movie {
+//	khai bao thuoc tinh class movie
+	private int id;
+	private String nameMovie;
+	private String director;
+	private String actor;
+	private Category category;
+	private String country;
+	private String link;
+	private boolean isVip;
+//	constructor movie
+	public Movie(int id, String nameMovie, String director, String actor, Category category, String country,
+			String link, boolean isVip) {
+		super();
+		this.id = id;
+		this.nameMovie = nameMovie;
+		this.director = director;
+		this.actor = actor;
+		this.category = category;
+		this.country = country;
+		this.link = link;
+		this.isVip = isVip;
+	}
+	
+}
